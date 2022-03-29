d38cf3c0658ff3eef9964ab98d599efbe73e9f0b
Pavel Lobtsov <plobtsov@gmail.com>
2022-03-29 21:32:14 +0300

    task8 finished

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  const formElements = event.currentTarget.elements;
+  const email = event.currentTarget.elements.email.value;
+  const password = event.currentTarget.elements.password.value;
+
+  if (email === "" || password === "") {
+    alert("Все поля должны быть заполнены!");
+  } else {
+    const values = {
+      email: email,
+      password: password,
+    };
+    event.currentTarget.reset();
+    return console.log(values);
+  }
+}
