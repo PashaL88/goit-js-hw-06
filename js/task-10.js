3b26c87ad7c9d53ae99335d3103fdb19ac4bf94b
Pavel Lobtsov <plobtsov@gmail.com>
2022-03-30 17:50:07 +0300

    Task 10 finished
    Homework finished

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,27 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const inputEl = document.querySelector('[type = "number"]');
+const createBtnEL = document.querySelector("[data-create]");
+const destroyBtnEL = document.querySelector("[data-destroy]");
+const boxesEl = document.querySelector("#boxes");
+
+function createBoxes(amount) {
+  destroyBoxes();
+
+  amount = inputEl.value;
+  let string = "";
+  let boxSize = 30;
+  for (let i = 0; i < amount; i += 1) {
+    string += `<div style='width:${boxSize}px; height:${boxSize}px; margin:10px; background-color:${getRandomHexColor()}'></div>`;
+    boxSize += 5;
+  }
+  boxesEl.insertAdjacentHTML("beforeend", string);
+}
+
+createBtnEL.addEventListener("click", createBoxes);
+destroyBtnEL.addEventListener("click", destroyBoxes);
+
+function destroyBoxes(event) {
+  boxesEl.innerHTML = "";
+}
