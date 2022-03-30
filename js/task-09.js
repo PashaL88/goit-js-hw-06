57647bf4a328463c274ef2a636bc9e87d041e435
Pavel Lobtsov <plobtsov@gmail.com>
2022-03-30 15:29:06 +0300

    task 9 finished

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeColorBtn = document.querySelector(".change-color");
+const colorEL = document.querySelector(".color");
+const bodyEl = document.querySelector("body");
+
+changeColorBtn.addEventListener("click", onchangeColorBtnClick);
+
+function onchangeColorBtnClick(event) {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  colorEL.textContent = bodyEl.style.background;
+  colorEL.textContent = getRandomHexColor();
+}
